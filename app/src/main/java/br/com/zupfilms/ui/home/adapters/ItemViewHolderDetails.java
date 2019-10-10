b6a15be412eac2e1c710071de1ef4f49bf4d5fd8
Tiago <tiagomnunes@hotmail.com>
2019-10-10 02:28:55 -0300

    fix bug

--- app/src/main/java/br/com/zupfilms/ui/home/adapters/ItemViewHolderDetails.java
+++ app/src/main/java/br/com/zupfilms/ui/home/adapters/ItemViewHolderDetails.java
@@ -104,7 +104,7 @@ class ItemViewHolderDetails extends RecyclerView.ViewHolder {
         }
         keywords.setText(sentenceBuilder(filmGenreList));
         this.movieDescription.setText(film.getOverview());
-        if(!film.getReleaseDate().isEmpty() && film.getReleaseDate().length()>=4){
+        if(film.getReleaseDate() != null && !film.getReleaseDate().isEmpty() && film.getReleaseDate().length()>=4){
             year.setText(film.getReleaseDate().substring(0, 4));
         }
         this.filmNote.setText(String.valueOf(film.getVoteAverage()));
